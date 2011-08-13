338b2771794e962c218583e5030e81e69de2da4c
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- url-watcher.py
+++ url-watcher.py
@@ -3,7 +3,7 @@
 # url-watcher
 # Monitor webpages for changes.
 #
-# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
 
 from simpletal import simpleTAL, simpleTALES
